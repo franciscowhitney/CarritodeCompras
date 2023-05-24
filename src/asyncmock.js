64f4cb2de2fdc751502482bb521a1581b58b632c
franciscowhitney <whitneyfran@gmail.com>
2023-05-24 18:39:00 -0300

    Estilos Agregados

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,7 +1,7 @@
 const productos = [
-    { nombre: "Chevy", precio: 2000000, id: "1", img: "../img/auto1.jpg", categoryName: "Clasico" },
-    { nombre: "Mustang", precio: 350000, id: "2", img: "../img/auto2.jpg", categoryName: "Clasico" },
-    { nombre: "Porche", precio: 350000, id: "3", img: "../img/auto3.jpg", categoryName: "Clasico" },
+    { nombre: "Chevy 75", precio: 2000000, id: "1", img: "../img/auto1.jpg", categoryName: "Clásico" },
+    { nombre: "Mustang 78", precio: 350000, id: "2", img: "../img/auto2.jpg", categoryName: "Clásico" },
+    { nombre: "Porche 90", precio: 350000, id: "3", img: "../img/auto3.jpg", categoryName: "Clásico" },
     { nombre: "Taos", precio: 50000, id: "4", img: "../img/auto4.jpg", categoryName: "Nuevos" },
     { nombre: "Volvo", precio: 50000, id: "5", img: "../img/auto5.jpg", categoryName: "Nuevos" },
     { nombre: "Harley", precio: 70000, id: "6", img: "../img/moto1.jpg", categoryName: "Motos" },
