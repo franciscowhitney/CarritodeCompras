f9839d02ce5d8ca5375d3fa579608b6d5d069506
franciscowhitney <whitneyfran@gmail.com>
2023-06-18 10:50:53 -0300

    campo descripcion agregado

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -8,7 +8,7 @@ import PageWrapper from '../common/PageWrapper'
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom'
 
-const ItemDetail = ({ id, nombre, precio, img, stock, hasError }) => {
+const ItemDetail = ({ id, nombre, precio, img, stock, descripcion, hasError }) => {
 
   const navigate = useNavigate()
   const [agregarCantidad, setAgregarCantidad] = useState(0);
@@ -44,7 +44,7 @@ const ItemDetail = ({ id, nombre, precio, img, stock, hasError }) => {
             <h2>Nombre: {nombre} </h2>
             <h3>Precio: {precio} </h3>
             <h3>ID: {id} </h3>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio eveniet facere maiores quo tempore quisquam! Consectetur dolores quos ducimus maiores quam quae, eveniet voluptatibus beatae, nemo cumque tempore modi?</p>
+            <p> <h3>Descripcion</h3> {descripcion}</p>
             <img src={img} alt={nombre} />
             <br />
             {
